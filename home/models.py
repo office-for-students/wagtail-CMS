09c9cf2561604d1311c46045934b4864957456dc
Michael Norton <michaelnorton@live.co.uk>
2019-04-30 10:36:09 +0100

    Update homepage fields in CMS and design

--- home/models.py
+++ home/models.py
@@ -6,22 +6,12 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    header_title = RichTextField(blank=True)
-    header_subtitle = RichTextField(blank=True)
-    info_headline = RichTextField(blank=True)
-    info_points = RichTextField(blank=True)
-    help_title = RichTextField(blank=True)
-    help_text = RichTextField(blank=True)
-    find_title = RichTextField(blank=True)
-    find_text = RichTextField(blank=True)
+    header = RichTextField(blank=True)
+    left_CTA = RichTextField(blank=True)
+    right_CTA = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('header_title', classname="full"),
-        FieldPanel('header_subtitle', classname="full"),
-        FieldPanel('info_headline', classname="full"),
-        FieldPanel('info_points', classname="full"),
-        FieldPanel('help_title', classname="full"),
-        FieldPanel('help_text', classname="full"),
-        FieldPanel('find_title', classname="full"),
-        FieldPanel('find_text', classname="full"),
+        FieldPanel('header', classname="full"),
+        FieldPanel('left_CTA', classname="full"),
+        FieldPanel('right_CTA', classname="full"),
     ]
