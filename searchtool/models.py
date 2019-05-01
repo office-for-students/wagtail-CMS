24507c1412acb38257c2da220222d57275fb379d
Michael Norton <michaelnorton@live.co.uk>
2019-05-01 12:09:28 +0100

    Add choose a uni page in search tool

--- searchtool/models.py
+++ searchtool/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import TextField
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField, RichTextField
@@ -19,4 +20,15 @@ class WhereToStudy(Page):
     content_panels = Page.content_panels + [
         FieldPanel('header', classname="full"),
         StreamFieldPanel('options', classname="full")
+    ]
+
+class ChooseAUni(Page):
+    header = RichTextField(blank=True)
+    placeholder = TextField(blank=True)
+    button = TextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('header', classname="full"),
+        FieldPanel('placeholder', classname="full"),
+        FieldPanel('button', classname="full")
     ]
