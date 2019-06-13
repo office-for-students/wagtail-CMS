7c23b891fb1799c8bbcd0a9175d4940c08f2c8cc
Michael Norton <michaelnorton@live.co.uk>
2019-06-13 09:50:05 +0200

    Update homepage design and add search form

--- home/models.py
+++ home/models.py
@@ -4,21 +4,25 @@ from wagtail.core.models import Page
 from wagtail.core.fields import StreamField, RichTextField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from django.db.models.fields import TextField
 
 
 class HomePage(Page):
     header = RichTextField(blank=True, features=['h1'])
-    subtitle = RichTextField(blank=True)
     page_links = StreamField([
         ('link', blocks.StructBlock([
             ('page', blocks.PageChooserBlock()),
-            ('title', blocks.CharBlock()),
-            ('subtitle', blocks.CharBlock())
+            ('title', blocks.CharBlock())
         ]))
     ])
+    search_header = RichTextField(blank=True, features=['h1'])
+    course_field_label = TextField(blank=True)
+    institution_field_label = TextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('header', classname="full"),
-        FieldPanel('subtitle', classname="full"),
-        StreamFieldPanel('page_links', classname="full")
+        StreamFieldPanel('page_links', classname="full"),
+        FieldPanel('search_header', classname="full"),
+        FieldPanel('course_field_label', classname="full"),
+        FieldPanel('institution_field_label', classname="full"),
     ]
