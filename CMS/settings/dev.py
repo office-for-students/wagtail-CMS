8610045bffc457bf4b3991a40ca79cc58a307556
Matt Nicks <matt.nicks@methods.co.uk>
2019-07-05 12:55:44 +0100

    setup local env variable for testing

--- CMS/settings/dev.py
+++ CMS/settings/dev.py
@@ -2,6 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+LOCAL = os.environ.get('LOCAL', False)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3z@sut7t&=hkx9*+b-h=&2$_ajcqvqrfcc0mc@*)y6c5v#6qsz'
