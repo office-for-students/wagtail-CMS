2ef6417327e7e2deaf3129523ea093331cd80748
Matt Nicks <matt.nicks@methods.co.uk>
2019-07-08 12:06:42 +0100

    create internal error model

--- errors/models.py
+++ errors/models.py
@@ -14,3 +14,16 @@ class ApiError:
 
     def get_error_message(self):
         return self.base_message % self.action
+
+
+class InternalError:
+    base_message = 'An error occurred whilst %s'
+
+    def __init__(self, status_code, details):
+        self.status_code = status_code
+        self.details = details
+
+        logger.error(self.get_error_message())
+
+    def get_error_message(self):
+        return self.base_message % self.details
