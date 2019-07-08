a82f1ebfcc1f0553943eaa6e49c95d010656b8e6
Matt Nicks <matt.nicks@methods.co.uk>
2019-07-08 09:01:22 +0100

    add initial request mocker

---
+++ CMS/test/mocks.py
@@ -0,0 +1,24 @@
+import json
+
+from requests.models import Response
+
+
+class StatusMocks:
+    HTTP_200_OK = 200
+    HTTP_400_BAD_REQUEST = 400
+    HTTP_404_NOT_FOUND = 404
+    HTTP_500_INTERNAL_SERVER_ERROR = 500
+
+
+class SearchMocks:
+
+    @classmethod
+    def get_search_response_content(cls):
+        return {}
+
+    @classmethod
+    def get_successful_search_response(cls):
+        response = Response()
+        response.status_code = StatusMocks.HTTP_200_OK
+        response._content = json.dumps(cls.get_search_response_content()).encode('utf-8')
+        return response
