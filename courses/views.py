1455b71539207702121a496fdee38a3aeaeccc07
mwnicks <matthew.nicks@methods.co.uk>
2019-07-17 10:25:43 +0100

    make the course details accessible in the template

--- courses/views.py
+++ courses/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 
-from courses.models import CourseDetailPage
+from courses.models import CourseDetailPage, Course
 
 
 def courses_detail(request, institution_id, course_id, kis_mode):
-    course = Course.find(institution_id, course_id, kis_mode)
+    course, error = Course.find(institution_id, course_id, kis_mode)
+
+    if error:
+        return render(request, '500.html')
 
     page = CourseDetailPage.objects.get()
 
     context = {
         'page': page,
+        'course': course,
     }
 
     return render(request, 'courses/course_detail_page.html', context)
