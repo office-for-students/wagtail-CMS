5f1fb24693242d198dd624a9d3ad7ff73c64a69c
mwnicks <matthew.nicks@methods.co.uk>
2019-07-17 10:25:16 +0100

    do initial course detail data mapping

--- courses/models.py
+++ courses/models.py
@@ -5,6 +5,9 @@ from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.core import blocks
 
+from courses import request_handler
+from errors.models import ApiError
+from institutions.models import Institution
 
 DATA_SET_KEYS = (
     ('student_satisfaction', 'Student satisfaction'),
@@ -31,3 +34,129 @@ class CourseDetailPage(Page):
         StreamFieldPanel('accordions'),
         FieldPanel('uni_site_links_header'),
     ]
+
+
+class Course:
+    MODES = {
+        'Full-time': 1,
+        'Part-time': 2
+    }
+
+    def __init__(self, data_obj):
+        self.id = data_obj.get('id')
+        course_details = data_obj.get('course')
+        if course_details:
+            self.country = CourseCountry(course_details.get('country'))
+            self.distance_learning = CourseDistanceLearning(course_details.get('distance_learning'))
+            self.foundation_year = CourseFoundationYear(course_details.get('foundation_year_availability'))
+            self.honours_award_provision = course_details.get('honours_award_provision')
+            self.institution = Institution(course_details.get('institution'))
+            self.kis_course_id = course_details.get('kis_course_id')
+            self.length = CourseLength(course_details.get('length_of_course'))
+            self.course_links = course_details.get('links')
+            self.locations = []
+            for location in course_details.get('locations'):
+                self.locations.append(CourseLocation(location))
+            self.mode = CourseMode(course_details.get('mode'))
+            self.qualification = CourseQualification(course_details.get('qualification'))
+            self.sandwich_year = CourseSandwichYear(course_details.get('sandwich_year'))
+            self.english_title = course_details.get('title').get('english')
+            self.welsh_title = course_details.get('title').get('welsh')
+            self.ucas_programme_id = course_details.get('ucas_programme_id')
+            self.year_abroad = CourseYearAbroad(course_details.get('year_abroad'))
+
+    @property
+    def number_of_locations(self):
+        return len(self.locations)
+
+    @property
+    def locations_list(self):
+        location_names = []
+        for location in self.locations:
+            location_names.append(location.english_name)
+        return ', '.join(location_names)
+
+    @classmethod
+    def find(cls, institution_id, course_id, mode):
+        course = None
+        error = None
+
+        response = request_handler.load_course_data(institution_id, course_id, mode)
+
+        if response.ok:
+            course = cls(response.json())
+        else:
+            error = ApiError(response.status_code, 'Loading details for course %s %s at %s' %
+                             (course_id, mode, institution_id))
+
+        return course, error
+
+    @staticmethod
+    def get_mode_code(mode):
+        return Course.MODES.get(mode)
+
+
+class CourseCountry:
+
+    def __init__(self, data_obj):
+        self.name = data_obj.get('name')
+        self.code = data_obj.get('code')
+
+
+class CourseDistanceLearning:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
+
+class CourseFoundationYear:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
+
+class CourseLength:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
+
+class CourseLocation:
+
+    def __init__(self, data_obj):
+        self.latitude = data_obj.get('latitude')
+        self.longitude = data_obj.get('longitude')
+        self.english_name = data_obj.get('name').get('english')
+        self.welsh_name = data_obj.get('name').get('welsh')
+
+
+class CourseMode:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+        
+
+class CourseQualification:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
+
+class CourseSandwichYear:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
+
+class CourseYearAbroad:
+
+    def __init__(self, data_obj):
+        self.code = data_obj.get('code')
+        self.label = data_obj.get('label')
+
--- institutions/models.py
+++ institutions/models.py
@@ -1,7 +1,7 @@
 class Institution:
 
     def __init__(self, data_obj):
-        self.pub_ukprn_name = data_obj.get('pub_ukprn_name')
+        self.pub_ukprn_name = data_obj.get('pub_ukprn_name').title()
         self.pub_ukprn = data_obj.get('pub_ukprn')
         self.ukprn_name = data_obj.get('ukprn_name')
         self.ukprn = data_obj.get('ukprn')
