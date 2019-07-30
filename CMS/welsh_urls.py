06b8b302381e5b261f6768446dc16977492486ec
mwnicks <matthew.nicks@methods.co.uk>
2019-07-30 14:46:58 +0100

    add pathing for welsh pages

---
+++ CMS/welsh_urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+
+from courses import urls as courses_urls
+from institutions import urls as institution_urls
+
+from search import views as search_views
+from coursefinder import views as coursefinder_views
+
+urlpatterns = [
+    url(r'^search/$', search_views.search, name='search'),
+    url(r'^results/$', coursefinder_views.results, name='results'),
+
+    url(r'^narrow-search/$', coursefinder_views.narrow_search, name='narrow_search'),
+    url(r'^course-finder/results/$', coursefinder_views.course_finder_results, name='course_finder_results'),
+
+    url(r'^course-details/', include(courses_urls)),
+    url(r'^institution-details/', include(institution_urls)),
+]
