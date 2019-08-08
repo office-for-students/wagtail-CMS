b7ecca5a2102618d58412c05a1a4209b03a041bf
mwnicks <matthew.nicks@methods.co.uk>
2019-08-08 18:37:25 +0100

    create functionality for building a menu structure

--- core/models.py
+++ core/models.py
@@ -1,4 +1,11 @@
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.snippets.models import register_snippet
+
 
 
 class DiscoverUniBasePage(Page):
@@ -10,3 +17,35 @@ class DiscoverUniBasePage(Page):
 
     class Meta:
         abstract = True
+
+
+class SimpleMenuItem(blocks.StructBlock):
+    label = blocks.CharBlock(required=False, help_text='Leave blank to use the page name')
+    link_page = blocks.PageChooserBlock(required=False)
+
+    class Meta:
+        icon = 'link'
+
+
+class MultiMenuItem(blocks.StructBlock):
+    label = blocks.CharBlock(required=True)
+    menu_items = blocks.StreamBlock([
+        ('simple_menu_item', SimpleMenuItem())
+    ], icon='arrow-left', label='Items')
+
+
+@register_snippet
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    menu_items = StreamField([
+        ('simple_menu_item', SimpleMenuItem()),
+        ('multi_menu_item', MultiMenuItem())
+    ])
+
+    panels = [
+        FieldPanel('name'),
+        StreamFieldPanel('menu_items')
+    ]
+
+    def __str__(self):
+        return self.name
