dc191c716d6a1cf7d3d80f208463acb04907ae9d
mwnicks <matthew.nicks@methods.co.uk>
2019-08-14 10:00:43 +0100

    handle tildes in the course ids in the url

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from courses.views import courses_detail
 
 urlpatterns = [
-    url(r'(?P<institution_id>[\w\-]+)/(?P<course_id>[\w\-]+)/(?P<kis_mode>[\w\-]+)/', courses_detail,
+    url(r'(?P<institution_id>[\w\-]+)/(?P<course_id>[\w\-\~]+)/(?P<kis_mode>[\w\-]+)/', courses_detail,
         name='courses_detail'),
 ]
