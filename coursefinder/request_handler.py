725996f3e2d73046ee5ba44d167d338e8fb05afb
mwnicks <matthew.nicks@methods.co.uk>
2019-08-15 11:45:27 +0100

    update search url structure to match new api

--- coursefinder/request_handler.py
+++ coursefinder/request_handler.py
@@ -11,7 +11,7 @@ def query_course_and_institution(course, institution, limit, offset):
         headers = {
             'Ocp-Apim-Subscription-Key': settings.DATASETAPIKEY
         }
-        base_url = "%s/institution-courses?limit=%s&offset=%s&q=%s&institutions=%s"
+        base_url = "%s?limit=%s&offset=%s&qc=%s&qi=%s"
         return requests.get(url=base_url % (settings.SEARCHAPIHOST, limit, offset, course, institution),
                             headers=headers)
 
@@ -23,11 +23,11 @@ def course_finder_query(subject, institution, mode, countries, limit, offset):
         headers = {
             'Ocp-Apim-Subscription-Key': settings.DATASETAPIKEY
         }
-        url = "%s/institution-courses?limit=%s&offset=%s" % (settings.SEARCHAPIHOST, limit, offset)
+        url = "%s?limit=%s&offset=%s" % (settings.SEARCHAPIHOST, limit, offset)
         if subject and subject != '':
-            url = f"{url}&subjects={subject}"
+            url = f"{url}&qc={subject}"
         if institution and institution != '':
-            url = f"{url}&institutions={institution}"
+            url = f"{url}&qi={institution}"
         if mode and 'Full-time,Part-time' not in mode and mode != '':
             url = f"{url}&filters={mode.lower().replace('-', '_').replace(' ', '_')}"
         if countries and countries != '':
