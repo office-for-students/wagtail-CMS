3c15b2b9857da8310ece4f035bf677d16ac6959c
mwnicks <matthew.nicks@methods.co.uk>
2019-09-04 18:31:08 +0100

    make trailing slashes optional in the urls

--- institutions/urls.py
+++ institutions/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from institutions.views import institution_detail
 
 urlpatterns = [
-    url(r'(?P<institution_id>[\w\-]+)/', institution_detail, name='institution_detail'),
+    url(r'(?P<institution_id>[\w\-]+?)/', institution_detail, name='institution_detail'),
 ]
