76fb2da264cd82d4f5274d40e0f19f1b95288b5b
mwnicks <matthew.nicks@methods.co.uk>
2019-09-04 09:25:07 +0100

    implement initial js to mark course as selected on page load if in basket

---
+++ courses/static/js/compare-selector.js
@@ -0,0 +1,68 @@
+(function ($) {
+
+    var CompareSelector = function(button) {
+        this.button = $(button);
+        this.setup();
+    }
+
+    CompareSelector.prototype = {
+        setup: function() {
+            this.courseSelected = false;
+
+            var url = location.pathname.split('/');
+            if (url[1] === 'cy') {
+                this.uniId = url[3];
+                this.courseId = url[4];
+                this.mode = url[5];
+            } else {
+                this.uniId = url[2];
+                this.courseId = url[3];
+                this.mode = url[4];
+            }
+
+            this.loadSelectedCourses();
+            this.setInitialView();
+            this.startWatcher();
+        },
+
+        loadSelectedCourses: function() {
+            if (JSON.parse(localStorage.getItem('comparisonCourses'))) {
+                this.selectedCourses = JSON.parse(localStorage.getItem('comparisonCourses'));
+            } else {
+                this.selectedCourses = [];
+            }
+            this.setCourseSelected();
+        },
+
+        setCourseSelected: function() {
+            for (var i = 0; i < this.selectedCourses.length; i++) {
+                var course = this.selectedCourses[i];
+                if (course.uniId === this.uniId && course.courseId === this.courseId && course.mode === this.mode) {
+                    this.courseSelected = true;
+                }
+            }
+        },
+
+        setInitialView: function() {
+            if (this.courseSelected) {
+                this.button.addClass('selected');
+            }
+        },
+
+        startWatcher: function() {
+            this.button.click(function() {
+                console.log('adding to basket');
+            })
+        }
+    }
+
+    function init() {
+        var compareBtns = $('.course-detail__compare-btn');
+        for (var i = 0; i < compareBtns.length; i++) {
+            new CompareSelector(compareBtns[i]);
+        }
+    }
+
+    $(document).on('page:load', init);
+    $(init)
+}(jQuery))
