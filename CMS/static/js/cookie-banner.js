461ff7733f8bfa7c150489d230b06911c6799cf7
mwnicks <matthew.nicks@methods.co.uk>
2019-09-08 19:09:27 +0100

    hide banner on accept click

--- CMS/static/js/cookie-banner.js
+++ CMS/static/js/cookie-banner.js
@@ -14,8 +14,11 @@
         },
 
         startWatchers: function() {
+            var that = this;
+            
             this.acceptBtn.click(function() {
                 document.cookie = "discoverUniCookies=accepted; expires=Thu, 31 Dec 2050 23:59:59 UTC";
+                that.wrapper.hide();
             });
 
             this.findOutMoreBtn.click(function() {
