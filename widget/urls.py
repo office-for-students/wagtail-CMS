b280a6c44ab3d9c034a8b2de1a6b53c3b8bb9339
mwnicks <matthew.nicks@methods.co.uk>
2019-09-09 19:05:15 +0100

    widget iframe accest no mode and default to FullTime

--- widget/urls.py
+++ widget/urls.py
@@ -5,5 +5,7 @@ from widget.views import widget_iframe, widget_embed
 urlpatterns = [
     url(r'(?P<uk_prn>[\w\-]+?)/(?P<kis_course_id>[\w\-\~]+?)/(?P<orientation>[\w\-]+?)/(?P<size>[\w\-]+?)/'
         r'(?P<language>[\w\-]+?)/(?P<kis_mode>[\w\-]+?)/$(?i)', widget_iframe, name='widget_iframe'),
+    url(r'(?P<uk_prn>[\w\-]+?)/(?P<kis_course_id>[\w\-\~]+?)/(?P<orientation>[\w\-]+?)/(?P<size>[\w\-]+?)/'
+        r'(?P<language>[\w\-]+?)/$(?i)', widget_iframe, name='widget_iframe'),
     url(r'^embed-script/$(?i)', widget_embed, name='widget_embed'),
 ]
