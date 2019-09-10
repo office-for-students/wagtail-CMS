84d12ae105c13e5b62568341cc00db2fa12e8b8d
mwnicks <matthew.nicks@methods.co.uk>
2019-09-10 19:06:53 +0100

    remove old functions

--- CMS/static/js/main.js
+++ CMS/static/js/main.js
@@ -21,10 +21,6 @@ $(document).ready(function() {
 
     }
 
-  function handleStartAgain() {
-    sessionStorage.clear()
-  }
-
   if (sessionStorage.getItem("countries") != null) {
     let countries = sessionStorage.getItem("countries")
     $('#countries').text(countries.split(",").join(", "))
@@ -57,18 +53,4 @@ $(document).ready(function() {
     let postcode = sessionStorage.getItem("postcode")
     $('#narrow').text(postcode.split(",").join(", "))
   }
-
-//  function handleResultsRequest() {
-//    let subject_query = sessionStorage.getItem('subjectCodes')
-//    let institution_query = sessionStorage.getItem('uni')
-//    let mode_query = sessionStorage.getItem('modes')
-//    let countries_query = sessionStorage.getItem('countries')
-//    let postcode_query = sessionStorage.getItem('postcode')
-//
-//    $("input[name='subject_query']").val(subject_query)
-//    $("input[name='institution_query']").val(institution_query)
-//    $("input[name='mode_query']").val(mode_query)
-//    $("input[name='countries_query']").val(countries_query)
-//    $("input[name='postcode_query']").val(postcode_query)
-//  }
 });
