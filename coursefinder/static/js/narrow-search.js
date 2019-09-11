4fd5030b5e2aba3bd8451b9a1da4140d89dafdaf
mwnicks <matthew.nicks@methods.co.uk>
2019-09-11 15:21:38 +0100

    clear narrow options on landing on narrow page

--- coursefinder/static/js/narrow-search.js
+++ coursefinder/static/js/narrow-search.js
@@ -9,6 +9,8 @@
         setup: function() {
             this.btn = this.wrapper.find('.course-finder-content__next-button');
             this.form = this.wrapper.find('.course-finder-content__question-form');
+            sessionStorage.setItem('uni', '')
+            sessionStorage.getItem('postcode', '')
 
             this.startWatchers();
         },
@@ -22,16 +24,12 @@
 
             this.form.submit(function() {
                 var subject_query = sessionStorage.getItem('subjectCodes')
-                var institution_query = sessionStorage.getItem('uni')
                 var mode_query = sessionStorage.getItem('modes')
                 var countries_query = sessionStorage.getItem('countries')
-                var postcode_query = sessionStorage.getItem('postcode')
 
                 $("input[name='subject_query']").val(subject_query)
-                $("input[name='institution_query']").val(institution_query)
                 $("input[name='mode_query']").val(mode_query)
                 $("input[name='countries_query']").val(countries_query)
-                $("input[name='postcode_query']").val(postcode_query)
             });
         }
     }
