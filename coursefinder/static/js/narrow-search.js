e16fcf62e438b9b84555c63e50d8fc31dbc1adaf
mwnicks <matthew.nicks@methods.co.uk>
2019-09-11 15:44:13 +0100

    fill the forms on narrow search init so the skip form is filled  as well as the main form

--- coursefinder/static/js/narrow-search.js
+++ coursefinder/static/js/narrow-search.js
@@ -12,6 +12,14 @@
             sessionStorage.setItem('uni', '')
             sessionStorage.getItem('postcode', '')
 
+            var subject_query = sessionStorage.getItem('subjectCodes');
+            var mode_query = sessionStorage.getItem('modes');
+            var countries_query = sessionStorage.getItem('countries');
+
+            $("input[name='subject_query']").val(subject_query);
+            $("input[name='mode_query']").val(mode_query);
+            $("input[name='countries_query']").val(countries_query);
+
             this.startWatchers();
         },
 
@@ -20,16 +28,6 @@
 
             this.btn.click(function() {
                 that.form.submit();
-            });
-
-            this.form.submit(function() {
-                var subject_query = sessionStorage.getItem('subjectCodes')
-                var mode_query = sessionStorage.getItem('modes')
-                var countries_query = sessionStorage.getItem('countries')
-
-                $("input[name='subject_query']").val(subject_query)
-                $("input[name='mode_query']").val(mode_query)
-                $("input[name='countries_query']").val(countries_query)
             });
         }
     }
