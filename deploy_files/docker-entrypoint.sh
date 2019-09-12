836fa5623f37c75c977d74a7fb3dbc18088babbe
mwnicks <matthew.nicks@methods.co.uk>
2019-09-12 16:38:23 +0100

    undo worker changes

--- deploy_files/docker-entrypoint.sh
+++ deploy_files/docker-entrypoint.sh
@@ -11,13 +11,12 @@ done
 exec gunicorn CMS.wsgi:application \
   --name discover-uni-cms \
   --bind 0.0.0.0:80 \
-  --workers 10 \
+  --workers 3 \
   --log-level=info \
   --log-file=- \
   --access-logfile=- \
   --error-logfile=- \
-  --timeout 60 \
-  --max-requests 1000
+  --timeout 60
 
 
 # EXECUTE DOCKER COMMAND NOW
