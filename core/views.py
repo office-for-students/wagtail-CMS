4f7057398e646d172c7872cc8e5b842e7ede8267
mwnicks <matthew.nicks@methods.co.uk>
2019-09-17 12:00:54 +0100

    update feedback parsing to force new form in to old form API

--- core/views.py
+++ core/views.py
@@ -7,17 +7,18 @@ from core.request_handler import send_feedback
 
 def submit_feedback(request):
     post_body = request.POST
-    feedback_obj = {'questions': []}
+
+    # Always returns is_useful True as this field is required by the API but no longer in the form
+    feedback_obj = {'is_useful': True, 'questions': []}
     for key, value in post_body.items():
-        if key == 'is_useful':
-            feedback_obj[key] = value == 'yes'
-        elif key == 'page':
+        if key == 'page':
             feedback_obj[key] = value
-        elif key != 'csrfmiddlewaretoken':
+        elif key == 'user_feedback':
             feedback_obj.get('questions').append({
-                'title': key,
+                'title': 'howwasthisuseful',
                 'feedback': value
             })
+            # Always returns title howwasthisuseful as this field is expected by the API and the  current one isn't
 
     response = send_feedback(json.dumps(feedback_obj))
 
