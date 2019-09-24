4c6464dd683ef2834598c3e84520526340dbd8e1
danmorley <dan_morley@yahoo.com>
2019-09-24 18:34:03 +0100

    added polyfill for ie

--- CMS/static/js/main.js
+++ CMS/static/js/main.js
@@ -1,4 +1,20 @@
 (function ($) {
+    
+    // ie polyfill for Includes
+    if (!String.prototype.includes) {
+      String.prototype.includes = function(search, start) {
+        if (typeof start !== 'number') {
+          start = 0;
+        }
+
+        if (start + search.length > this.length) {
+          return false;
+        } else {
+          return this.indexOf(search, start) !== -1;
+        }
+      };
+    }
+    
     // ACCORDION
     var Accordion = function(wrapper) {
         this.wrapper = $(wrapper);
