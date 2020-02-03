63e247035c87dda5254d5d9973b6205bb8e462e2
ponofrei <onofreipaul@gmail.com>
2020-02-03 19:06:23 +0200

    Added local jsonfiles for testing purposes.

--- core/request_handler.py
+++ core/request_handler.py
@@ -1,7 +1,7 @@
+import json, os
 import requests
 
 from django.conf import settings
-
 
 def send_feedback(feedback_data):
     headers = {
@@ -12,6 +12,23 @@ def send_feedback(feedback_data):
 
 
 def get_json_file(json_file):
+
+    if settings.LOCAL:
+        print("Loading local jsonfile")
+        with open(os.path.join(settings.BASE_DIR, 'CMS/static/jsonfiles/' + json_file)) as json_file:
+            data = json_file.read()
+
+        response = requests.Response()
+
+        response_state = response.__getstate__()
+        response_state["status_code"] = 200
+        response_state["_content"] = data.encode('utf-8')
+        response_state["encoding"] = 'utf-8'
+        
+        response.__setstate__(response_state)
+
+        return response
+        
     headers = {
         'Ocp-Apim-Subscription-Key': settings.DATASETAPIKEY
     }
