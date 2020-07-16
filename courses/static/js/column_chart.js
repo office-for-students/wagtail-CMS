0ae9e6fac1dffb974f51e7839280921cac5a1ef5
Siobhan Stephens <siobhan.stephens@mobilise.cloud>
2020-07-16 09:58:50 +0100

    wip column chart

--- courses/static/js/column_chart.js
+++ courses/static/js/column_chart.js
@@ -76,21 +76,21 @@ $(function () {
             var chart = this.target.find('svg');
             chart.attr('role', 'img');
 
-            // var titleNode = document.createElement('title');
-            // var titleId = this.label + '-title';
-            // titleNode.setAttribute('id', titleId);
-            // var title = document.createTextNode(this.title);
-            // titleNode.appendChild(title);
+            var titleNode = document.createElement('title');
+            var titleId = this.label + '-title';
+            titleNode.setAttribute('id', titleId);
+            var title = document.createTextNode(this.title);
+            titleNode.appendChild(title);
 
-            // var descNode = document.createElement('desc');
-            // var descId = this.label + '-desc';
-            // descNode.setAttribute('id', descId);
-            // var desc = document.createTextNode(this.value + '%' + this.desc);
-            // descNode.appendChild(desc);
+            var descNode = document.createElement('desc');
+            var descId = this.label + '-desc';
+            descNode.setAttribute('id', descId);
+            var desc = document.createTextNode(this.value + '%' + this.desc);
+            descNode.appendChild(desc);
 
-            // chart.prepend(descNode);
-            // chart.prepend(titleNode);
-            // chart.attr('aria-labelledby', titleId + ' ' + descId);
+            chart.prepend(descNode);
+            chart.prepend(titleNode);
+            chart.attr('aria-labelledby', titleId + ' ' + descId);
         }
     }
 
