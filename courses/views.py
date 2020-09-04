ece285564136ffefad56c5866d01cefed31d1f92
rtrokosz <robert.rokosz@mobilise.cloud>
2020-09-04 13:20:20 +0100

    Tab logic working with SalariesAggregate objects.

--- courses/views.py
+++ courses/views.py
@@ -17,6 +17,7 @@ def regional_earnings(request):
         institution_id = request.POST['institution_id']
         course_id = request.POST['course_id']
         kis_mode = request.POST['kis_mode']
+        subject_code = request.POST['subject_code']
         course, error = Course.find(institution_id, course_id, kis_mode, language=enums.languages.ENGLISH)
 
         with open("./CMS/static/jsonfiles/regions.json", "r") as f:
@@ -34,8 +35,10 @@ def regional_earnings(request):
         def format_thousands(earnings):
             return f'{int(earnings):,}'
 
-        unavail_msgs_go = course.salaries_sector[0].display_unavailable_info()
-        unavail_msgs_leo = course.salaries_sector[1].display_unavailable_info()
+        salaries_aggregate = [element for element in course.salary_aggregates if element.subject_code == subject_code][0]
+
+        unavail_msgs_go = salaries_aggregate.aggregated_salaries_sector[0].display_unavailable_info()
+        unavail_msgs_leo = salaries_aggregate.aggregated_salaries_sector[1].display_unavailable_info()
         salary_sector_15_unavail_text = ""
         salary_sector_3_unavail_text = ""
         salary_sector_5_unavail_text = ""
@@ -50,17 +53,17 @@ def regional_earnings(request):
         if unavail_msgs_leo['unavailable_region_is_ni'] != "":
             unavailable_region_is_ni = unavail_msgs_leo['unavailable_region_is_ni']
 
-        if getattr(course.salaries_sector[0], "med"+region) == "" or getattr(course.salaries_sector[0], "med"+region) is None:
+        if getattr(salaries_aggregate.aggregated_salaries_sector[0], "med"+region) == "" or getattr(salaries_aggregate.aggregated_salaries_sector[0], "med"+region) is None:
             salary_sector_15_unavail_text = unavailable_region_not_exists
         elif region not in ('_uk', '_e', '_s', '_w', '_ni'):
             salary_sector_15_unavail_text = unavailable_region_not_nation
 
-        if getattr(course.salaries_sector[1], "med"+region) == "" or getattr(course.salaries_sector[1], "med"+region) is None:
+        if getattr(salaries_aggregate.aggregated_salaries_sector[1], "med"+region) == "" or getattr(salaries_aggregate.aggregated_salaries_sector[1], "med"+region) is None:
             salary_sector_3_unavail_text = unavailable_region_not_exists
         elif region == '_ni':
             salary_sector_3_unavail_text = unavailable_region_is_ni
 
-        if getattr(course.salaries_sector[2], "med"+region) == "" or getattr(course.salaries_sector[2], "med"+region) is None:
+        if getattr(salaries_aggregate.aggregated_salaries_sector[2], "med"+region) == "" or getattr(salaries_aggregate.aggregated_salaries_sector[2], "med"+region) is None:
             salary_sector_5_unavail_text = unavailable_region_not_exists
         elif region == '_ni':
             salary_sector_5_unavail_text = unavailable_region_is_ni
@@ -73,26 +76,26 @@ def regional_earnings(request):
             'of_those_asked_text': DICT.get('of those asked').get('en'),
             'region_full_name': region_full_name,
 
-            'salary_sector_15_med': format_thousands(getattr(course.salaries_sector[0], "med"+region)),
-            'salary_sector_15_lq': format_thousands(getattr(course.salaries_sector[0], "lq" + region)),
-            'salary_sector_15_uq': format_thousands(getattr(course.salaries_sector[0], "uq" + region)),
-            'salary_sector_15_pop': getattr(course.salaries_sector[0], "pop" + region),
-            'salary_sector_15_resp': getattr(course.salaries_sector[0], "resp" + region),
+            'salary_sector_15_med': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[0], "med"+region)),
+            'salary_sector_15_lq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[0], "lq" + region)),
+            'salary_sector_15_uq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[0], "uq" + region)),
+            'salary_sector_15_pop': getattr(salaries_aggregate.aggregated_salaries_sector[0], "pop" + region),
+            'salary_sector_15_resp': getattr(salaries_aggregate.aggregated_salaries_sector[0], "resp" + region),
             'salary_sector_15_unavail_text': salary_sector_15_unavail_text,
 
-            'salary_sector_3_med': format_thousands(getattr(course.salaries_sector[1], "med"+region)),
-            'salary_sector_3_lq': format_thousands(getattr(course.salaries_sector[1], "lq" + region)),
-            'salary_sector_3_uq': format_thousands(getattr(course.salaries_sector[1], "uq" + region)),
-            'salary_sector_3_pop': getattr(course.salaries_sector[1], "pop" + region),
+            'salary_sector_3_med': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[1], "med"+region)),
+            'salary_sector_3_lq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[1], "lq" + region)),
+            'salary_sector_3_uq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[1], "uq" + region)),
+            'salary_sector_3_pop': getattr(salaries_aggregate.aggregated_salaries_sector[1], "pop" + region),
             'salary_sector_3_unavail_text': salary_sector_3_unavail_text,
 
-            'salary_sector_5_med': format_thousands(getattr(course.salaries_sector[2], "med"+region)),
-            'salary_sector_5_lq': format_thousands(getattr(course.salaries_sector[2], "lq" + region)),
-            'salary_sector_5_uq': format_thousands(getattr(course.salaries_sector[2], "uq" + region)),
-            'salary_sector_5_pop': getattr(course.salaries_sector[2], "pop" + region),
+            'salary_sector_5_med': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[2], "med"+region)),
+            'salary_sector_5_lq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[2], "lq" + region)),
+            'salary_sector_5_uq': format_thousands(getattr(salaries_aggregate.aggregated_salaries_sector[2], "uq" + region)),
+            'salary_sector_5_pop': getattr(salaries_aggregate.aggregated_salaries_sector[2], "pop" + region),
             'salary_sector_5_unavail_text': salary_sector_5_unavail_text,
 
-            'inst_prov_pc': getattr(course.salaries_inst[0], 'prov_pc' + region)
+            'inst_prov_pc': getattr(salaries_aggregate.aggregated_salaries_inst[0], 'prov_pc' + region)
         }
         return JsonResponse(resp)
     else:
