8163c9c70fb9633e907e4b27d4a3f7c93fb170dc
Siobhan Stephens <siobhan.stephens@mobilise.cloud>
2020-10-13 10:00:11 +0100

    Number of courses bookmarked # on home page missing

---
+++ CMS/static/js/home-page.js
@@ -0,0 +1,39 @@
+(function ($) {
+
+    var HomePage = function(wrapper) {
+        this.wrapper = $(wrapper);
+        this.setup();
+    }
+
+    HomePage.prototype = {
+        setup: function() {
+            this.courseCountSpan = this.wrapper.find('.count');
+            this.selectedCourses = [];
+
+            this.loadSelectedCourses();
+            this.setInitialView();
+        },
+
+        loadSelectedCourses: function() {
+            if (JSON.parse(localStorage.getItem('comparisonCourses'))) {
+                this.selectedCourses = JSON.parse(localStorage.getItem('comparisonCourses'));
+            } else {
+                this.selectedCourses = [];
+            }
+        },
+
+        setInitialView: function() {
+            this.courseCountSpan.text(this.selectedCourses.length);
+        },
+
+    }
+   
+    function init() {
+        var bookmarkWrapper = $('.home-page__bookmark-body');
+        new HomePage(bookmarkWrapper[0]);
+        
+    }
+
+    $(document).on('page:load', init);
+    $(init)
+}(jQuery))
