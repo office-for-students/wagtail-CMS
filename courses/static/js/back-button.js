d9ea420ac0f121295d8da6572631238dab705c8d
Siobhan Stephens <siobhan.stephens@mobilise.cloud>
2020-11-06 14:44:00 +0000

    back button fix

--- courses/static/js/back-button.js
+++ courses/static/js/back-button.js
@@ -1,6 +1,5 @@
 (function ($) {
     function init() {
-        var form = $("#back_to_search");
         var backBtn = $("#course-detail__nav-control-back");
         backBtn.click(function(evt) {
             window.history.back();
