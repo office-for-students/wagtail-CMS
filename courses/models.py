78198d9dd794a2bb57bfc28e42b26a6208887e2e
Alan Warren <alan.warren@mobilise.cloud>
2020-11-12 12:58:53 +0000

    cd

--- courses/models.py
+++ courses/models.py
@@ -1693,6 +1693,12 @@ class Job:
         if job_data:
             self.job = fallback_to(job_data.get('job'), '')
             self.percentage = fallback_to(job_data.get('percentage_of_students'),0)
+
+            if self.display_language == enums.languages.ENGLISH:
+                self.percentage = self.percentage.replace('<5', 'Less than 5')
+            else:
+                self.percentage = self.percentage.replace('<5', 'Llai na 5')
+
             self.order = job_data.get('order')
             self.hs = fallback_to(job_data.get('hs'), '')
 
