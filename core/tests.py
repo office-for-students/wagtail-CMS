280f457f854202092a84c9a96e26c2261736202f
sevenearths <from_a_far@hushmail.com>
2021-03-03 11:31:03 +0000

    added base tests

--- core/tests.py
+++ core/tests.py
@@ -1,15 +1,19 @@
 from django.core import management
 from django.test import TestCase, Client
+from django.core.management.base import CommandError
 from django.contrib.contenttypes.models import ContentType
 
 from wagtail.core.models import Page
 
 from CMS.enums import enums
+from core.mongo import Mongo
 from CMS.test.factories import PageFactory
 from CMS.test.utils import UniSimpleTestCase
 from core.utils import get_page_for_language
 from coursefinder.models import CourseFinderChooseCountry
 from site_search.models import SearchLandingPage
+
+from django.conf import settings
 
 
 class CoreUtilsTests(UniSimpleTestCase):
@@ -99,15 +103,24 @@ class CoreModelsTests(UniSimpleTestCase):
 
 
 class BaseTestCase(TestCase):
-
-    client = None
+    mongo_host      = 'mongo'
+    mongo_username  = 'mongodb'
+    mongo_password  = 'mongodb'
+    client          = None
+    institution_id  = 10005343
 
     def __init__(self, *args, **kwargs):
-
         super().__init__(*args, **kwargs)
         self.client = Client()
 
-    def setup(self):
-        Page.objects.all().delete()
-        ContentType.objects.all().delete()
-        management.call_command('loaddata', 'CMS/fixtures/postgres.json')
+    def populate_mongodb(self):
+        if settings.MONGODB_HOST == self.mongo_host and \
+           settings.MONGODB_USERNAME == self.mongo_username and \
+           settings.MONGODB_PASSWORD == self.mongo_password:
+            mongo = Mongo('institutions')
+            result = mongo.get_one(
+                {'institution_id': str(self.institution_id)}
+            )
+            if result is None:
+                management.call_command('populate_institutions')
+                management.call_command('populate_courses')
