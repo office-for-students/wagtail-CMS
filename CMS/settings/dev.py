cdc33fdc4160efb8ab53e36622b44c9258884cb4
sevenearths <from_a_far@hushmail.com>
2021-03-03 14:04:04 +0000

    (changed the secret key to get it from the environment for dev)

--- CMS/settings/dev.py
+++ CMS/settings/dev.py
@@ -10,7 +10,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-SECRET_KEY = "y%_whg_blbor4(6e!j3qxin9_qma025j+u=mn27%u^pdw=n2b%"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 try:
     from .local import *
