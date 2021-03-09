5aac64cce4a4cdb9a24dabeb1ada863f2a5eb98c
sevenearths <from_a_far@hushmail.com>
2021-03-09 11:01:26 +0000

    added comment to dev secret_key

--- CMS/settings/dev.py
+++ CMS/settings/dev.py
@@ -10,6 +10,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
+# NOTE: this SECRET_KEY is ONLY used in testing, NOT used in production
 SECRET_KEY = '3z@sut7t&=hkx9*+b-h=&2$_ajcqvqrfcc0mc@*)y6c5v#6qsz'
 
 try:
