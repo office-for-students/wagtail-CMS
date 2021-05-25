c0af3317358399921327fa9ebe24d25e03ed56f4
Rhisiart ap Gwilym <rhisiart.ap.gwilym@mobilise.cloud>
2021-05-25 12:31:33 +0100

    Add SortBySubject class, unit tests and JSON

--- coursefinder/test/test_sort_by_subject.py
+++ coursefinder/test/test_sort_by_subject.py
@@ -1,3 +1,4 @@
+from coursefinder.sort_by_subject import SortBySubject
 import unittest
 
 #
@@ -7,7 +8,58 @@ import unittest
 # To run this test file only:
 #   python manage.py test --pattern="test_sort_by_subject.py"
 #
-class TestSortBySubject(unittest.TestCase):
+class TestGetLabelForSubject(unittest.TestCase):
 
-    def test_failing(self):
-        self.assertEqual(1, 2)
+    def test_convert_to_dictionary(self):
+        actual = SortBySubject.convert_to_dictionary(self, data)        
+        self.assertEqual(actual, expected)
+        self.assertEquals(actual['CAH17-01-08']['english_name'], 'Accounting')
+        self.assertEquals(actual['CAH02-04-02']['welsh_name'], 'Nyrsio oedolion')
+
+    def test_when_no_subject_found_return_None(self):
+        subject = 'something-that-does-not-exist'
+        actual = SortBySubject(data).get_labels(subject)
+        expected = None
+        self.assertEqual(actual, expected)
+        
+    def test_when_subject_found_return_labels(self):
+        subject = 'CAH02-04-02'
+        actual = SortBySubject(data).get_labels(subject)
+        self.assertEqual(actual['english_name'], 'Adult nursing')
+        self.assertEqual(actual['welsh_name'], 'Nyrsio oedolion')
+
+    def test_when_subject_found_return_english_label(self):
+        subject = 'CAH17-01-08'
+        actual = SortBySubject(data).get_label(subject)
+        self.assertEqual(actual, 'Accounting')
+
+    def test_when_subject_found_return_welsh_label(self):
+        subject = 'CAH17-01-08'
+        actual = SortBySubject(data).get_label_welsh(subject)
+        self.assertEqual(actual, 'Cyfrifeg')
+
+data = [
+    {
+        "code": "CAH17-01-08",
+        "english_name": "Accounting",
+        "welsh_name": "Cyfrifeg",
+        "level": "3"
+    },
+    {
+        "code": "CAH02-04-02",
+        "english_name": "Adult nursing",
+        "welsh_name": "Nyrsio oedolion",
+        "level": "3"
+    }
+]
+
+expected = {
+   "CAH17-01-08":{
+      "english_name":"Accounting",
+      "welsh_name":"Cyfrifeg",
+   },
+   "CAH02-04-02":{
+      "english_name":"Adult nursing",
+      "welsh_name":"Nyrsio oedolion",
+   }
+}
