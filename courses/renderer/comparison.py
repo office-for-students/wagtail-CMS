145ca78e736893b0868e97288f4ef77374a2c522
SimonMobi <85573550+SimonMobi@users.noreply.github.com>
2021-07-02 10:40:56 +0100

    Re-orgnise file format, remove completed todos and ensure language translation occurs.

--- courses/renderer/comparison.py
+++ courses/renderer/comparison.py
@@ -4,9 +4,78 @@ from typing import List
 from CMS import translations
 from CMS.translations import DICT
 from courses.models import Course
+import logging
+
+logger = logging.getLogger(__name__)
+
+STUDY_MODE = "study_mode"
+COURSE_LENGTH = "length"
+LOCATIONS = "locations"
+DISTANCE_LEARNING = "distance_learning"
+PLACEMENT_YEAR = "placement_year"
+YEAR_ABROAD = "year_abroad"
+FOUNDATION_YEAR = "foundation_year"
+PROFESSIONAL_ACCREDITATION = "professional_accreditation"
 
 
-# TODO: update these methods to use the translation.py to store the various translations to be consistent elsewhere
+def dict_for_comparison_view_for_courses(courses: List[Course], language="en") -> List[dict]:
+    response = []
+    context = dict()
+    context["course_details"] = dict(
+        title=subject_for_key("course_details", language),
+        dataset=course_details(courses, language)
+    )
+
+    response.append(context)
+    return response
+
+
+def empty_data_structure(key: str, language: str):
+    return dict(title=subject_for_key(key, language), values=[])
+
+
+def course_details(courses: List[Course], language: str):
+    primary_key = 0
+    action = 1
+    sections = [
+        (STUDY_MODE, presentable_course_mode),
+        (COURSE_LENGTH, presentable_course_length),
+        (LOCATIONS, presentable_course_locations),
+        (DISTANCE_LEARNING, presentable_distance_learning),
+        (PLACEMENT_YEAR, presentable_placement_year),
+        (YEAR_ABROAD, presentable_year_abroad),
+        (FOUNDATION_YEAR, presentable_foundation_year),
+        (PROFESSIONAL_ACCREDITATION, presentable_accreditation)
+    ]
+
+    section_1 = dict()
+    for section in sections:
+        section_1[section[primary_key]] = empty_data_structure(section[primary_key], language)
+
+    for course in courses:
+        # Study mode
+        # iterate through each required section (see sections = [...] for the course and append the response  to the
+        # corresponding (values eg `section["key"]["values"].append` array.
+        for section in sections:
+            section_1[section[primary_key]]["values"].append(
+                section[action](
+                    course,
+                    language
+                )
+            )
+        # TODO: add next section here
+
+    return section_1
+
+
+def create_dataset(action: Callable[[Course, str], str], course: Course, language: str):
+    return action(course, language)
+
+
+def subject_for_key(key, language):
+    response = DICT.get(key).get(language) if key in DICT else key
+    return response
+
 
 def presentable_course_mode(course, language):
     # Create course mode label as we want it presented in template
@@ -39,10 +108,12 @@ def presentable_course_locations(course, language=None):
 
 
 def presentable_distance_learning(course: Course, language: str):
-    if course.distance_learning.code:
+    if course.distance_learning.code == 1:
         return translations.OPTIONALS['yes'].get(language)
-    else:
+    elif course.distance_learning.code == 0:
         return translations.OPTIONALS["not_available"].get(language)
+    else:
+        logger.warning("Distance learning code not managed: {course.distance_learning.code}")
 
 
 def presentable_placement_year(course: Course, language: str):
@@ -65,6 +136,8 @@ def presentable_accreditation(course: Course, language: str):
 
 
 def string_for_code(code, language, error):
+    # called from multiple methods, if one requires a change
+    # in terms of the string returned for the code, consider a new method vs editing this one
     if code == 0:
         option = translations.OPTIONALS['not_available']
     elif code == 1:
@@ -72,72 +145,7 @@ def string_for_code(code, language, error):
     elif code == 2:
         option = translations.OPTIONALS['compulsory']
     else:
-        raise Exception(f"{error} {code}")
+        logger.warning(f"String for code not available: {code} {error}")
+        option = {}
 
-    return option[language]
-
-
-def subject_for_key(key, language):
-    response = DICT.get(key).get(language) if key in DICT else key
-    return response
-
-
-def dict_for_comparison_view_for_courses(courses: List[Course], language="en") -> List[dict]:
-    response = []
-    context = dict()
-    # TODO remember to get the correct translaction for course details
-    context["course_details"] = dict(title=subject_for_key("course_details", language),
-                                     dataset=course_details(courses, language))
-    response.append(context)
-    return response
-
-
-def empty_data_structure(key: str, language: str):
-    return dict(title=subject_for_key(key, language), values=[])
-
-
-STUDY_MODE = "study_mode"
-COURSE_LENGTH = "length"
-LOCATIONS = "locations"
-DISTANCE_LEARNING = "distance_learning"
-PLACEMENT_YEAR = "placement_year"
-YEAR_ABROAD = "year_abroad"
-FOUNDATION_YEAR = "foundation_year"
-PROFESSIONAL_ACCREDITATION = "professional_accreditation"
-
-
-def create_dataset(action: Callable[[Course, str], str], course: Course, language: str):
-    return action(course, language)
-
-
-def course_details(courses: List[Course], language: str):
-    primary_key = 0
-    dataset = 1
-    sections = [
-        (STUDY_MODE, presentable_course_mode),
-        (COURSE_LENGTH, presentable_course_length),
-        (LOCATIONS, presentable_course_locations),
-        (DISTANCE_LEARNING, presentable_distance_learning),
-        (PLACEMENT_YEAR, presentable_placement_year),
-        (YEAR_ABROAD, presentable_year_abroad),
-        (FOUNDATION_YEAR, presentable_foundation_year),
-        (PROFESSIONAL_ACCREDITATION, presentable_accreditation)
-    ]
-
-    section_1 = dict()
-    for section in sections:
-        section_1[section[primary_key]] = empty_data_structure(section[primary_key], language)
-
-    print(f"empty sections = {section_1}")
-
-    for course in courses:
-        # Study mode
-        for section in sections:
-            section_1[section[primary_key]]["values"].append(
-                section[dataset](
-                    course,
-                    language
-                )
-            )
-
-    return section_1
+    return option.get(language)
