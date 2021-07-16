f7cf2c667d49141b58e7e9a6b5e1e4e409674198
nickpatti <nickdpatti50@gmail.com>
2021-07-16 11:53:15 +0100

    list sub accordions

--- courses/renderer/comparison.py
+++ courses/renderer/comparison.py
@@ -1,19 +1,58 @@
 import logging
-from typing import Any, Type, Callable, Dict, List
+from typing import Any, Type, Callable, Dict, List, Tuple
 
 from CMS import translations
 
 from courses.models import Course
 from courses.renderer.sections import CourseDetailSection, SatisfactionSection
 from courses.renderer.sections.base import Section
+from courses.renderer.sections.satisfaction import SubSatisfactionSection
 
 logger = logging.getLogger(__name__)
+
+
+def get_accordion_dataset(title: str, dataset, call_to_action=[]):
+    response = dict(
+        title=title,
+        dataset=dataset,
+        call_to_action=call_to_action
+    )
+
+    return response
+
+
+def get_sub_accordion_dataset(courses, language):
+    list_sub_accordions = [
+        (translations.term_for_key(key="teaching_on_my_course", language=language), [4, 5, 6, 7]),
+        (translations.term_for_key(key="learning_opportunities", language=language), [8, 9, 10]),
+        (translations.term_for_key(key="assessment_and_feedback", language=language), [11, 12, 13, 14]),
+        (translations.term_for_key(key="academic_support", language=language), [15, 16, 17]),
+        (translations.term_for_key(key="organisation_and_management",
+                                   language=language), [18, 19, 20]),
+        (translations.term_for_key(key="learning_resources", language=language), [21, 22, 23]),
+        (translations.term_for_key(key="learning_community", language=language), [24, 25]),
+        (translations.term_for_key(key="student_voice", language=language), [26, 27, 28, 29])
+    ]
+    response = []
+
+    for accordion in list_sub_accordions:
+
+        response.append(dict(
+            title=accordion[0],
+            dataset=SubSatisfactionSection(
+                keys=accordion[1],
+                courses=courses,
+                language=language
+            )
+        ))
+
+    return response
 
 
 def dataset_for_comparison_view(courses: List[Course], language="en") -> List[dict]:
     response = []
     context = dict()
-    context["course_details"] = dict(
+    context["course_details"] = get_accordion_dataset(
         title=translations.term_for_key(key="course_details", language=language),
         dataset=get_details(CourseDetailSection, courses, language),
         call_to_action=[
@@ -29,7 +68,9 @@ def dataset_for_comparison_view(courses: List[Course], language="en") -> List[di
     context["student_satisfaction_course_overview_1"] = dict(
         title=translations.term_for_key(key="student_satisfaction_course_overview_1", language=language),
         dataset=get_details(SatisfactionSection, courses, language),
+        sub_accordions=get_sub_accordion_dataset(courses, language)
     )
+
 
     response.append(context)
     return response
--- courses/renderer/sections/satisfaction.py
+++ courses/renderer/sections/satisfaction.py
@@ -8,9 +8,9 @@ SATISFACTION_DATA_FROM_PEOPLE = "data_from_people"
 PERCENTAGE_THOSE_ASKED = "percent_of_those_asked"
 
 satisfaction_list = [
-    "question_27.agree_or_strongly_agree",
+    "question_27",
     "number_of_students",
-    "response_rate"
+    "response_rate",
 ]
 
 primary_key = 0
@@ -39,6 +39,33 @@ class SatisfactionSection(Section):
             (SATISFACTION_DATA_FROM_PEOPLE, satisfaction_list[1]),
             (PERCENTAGE_THOSE_ASKED, satisfaction_list[2])
         ]
+
+        for i in range(1, 27):
+            satisfaction_list.append("question_" + str(i))
+            self.sections.append(tuple(("nss_question_" + str(i), satisfaction_list[i + 2])))
+        return self.sections
+
+    def generate_dict(self) -> dict:
+        for course in self.courses:
+            for section in self.get_sections():
+                self.data[section[primary_key]]["values"].append(
+                    presentable_satisfaction(course, section[action], self.language)
+                )
+
+        return self.data
+
+
+class SubSatisfactionSection(Section):
+
+    def __init__(self, keys: List[str], courses, language='en'):
+        self.keys = keys
+        super().__init__(courses, language)
+
+    def get_sections(self) -> List[Tuple[Any, Any]]:
+        for i in self.keys:
+            satisfaction_list.append("question_" + str(i))
+            self.sections.append(tuple(("nss_question_" + str(i), satisfaction_list[i + 2])))
+
         return self.sections
 
     def generate_dict(self) -> dict:
