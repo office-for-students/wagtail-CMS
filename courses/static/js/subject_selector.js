d6287d1d9e96fa9066c4ab71b83b511ee5847722
nickpatti <nickdpatti50@gmail.com>
2021-08-05 09:20:53 +0100

    beginning of styling for the multiple subjects

---
+++ courses/static/js/subject_selector.js
@@ -0,0 +1,4 @@
+let SubjectView = function subjectView() {
+    this.container = this.getElementById("subject_wrapper");
+    this.subject = this.container.querySelector('.subject_wrapper__container-subjects-subject');
+}
