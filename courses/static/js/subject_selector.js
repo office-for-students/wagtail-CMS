8aa1deca857af48c9e40b3f2a3a02a8ccfdc0199
nickpatti <nickdpatti50@gmail.com>
2021-08-05 11:13:28 +0100

    Select subject set event listeners

--- courses/static/js/subject_selector.js
+++ courses/static/js/subject_selector.js
@@ -1,4 +1,19 @@
-let SubjectView = function subjectView() {
-    this.container = this.getElementById("subject_wrapper");
-    this.subject = this.container.querySelector('.subject_wrapper__container-subjects-subject');
+function removeSelected(element) {
+    let parent = element.target.parentElement
+    console.log(parent.children)
+    if (parent.children.length > 1) {
+        for (let i = 0; i < parent.children.length; i++) {
+            parent.children[i].classList.remove("selected");
+        }
+    }
+}
+
+function setEventListeners() {
+    let parents = document.getElementsByTagName("OL");
+    for (let i = 0; i < parents.length; i++) {
+        parents[i].addEventListener("click", function (e) {
+            removeSelected(e);
+            e.target.classList.add("selected");
+        });
+    }
 }
