1006955197bfd4140747e7e0683b36ee81b8d4d0
nickpatti <nickdpatti50@gmail.com>
2021-08-16 11:57:02 +0100

    changes to the backend for the earnings accordion

--- courses/renderer/sections/earnings.py
+++ courses/renderer/sections/earnings.py
@@ -1,22 +1,27 @@
 from typing import List, Tuple, Any, Dict
+
+from CMS import translations
+from courses.models import Course
 from courses.renderer.sections.base import Section
 
 AVERAGE_EARNINGS = "average_earnings_course_overview_1"
 INSTITUTION = "institution"
 DATA_FROM_PEOPLE = "data_from_people"
 NATIONAL_AVERAGE = "national_average"
+REGION = "regions"
 
 earnings_list = [
     "subject_title_in_local_language",
     "med",
     "pop",
     "salary_default_country_med",
+    "country",
     "salary_default_country_pop"
 ]
 
 primary_key = 1
 action = 2
-suffix_index = 3
+prefix_index = 3
 model_index = 4
 
 
@@ -33,7 +38,7 @@ class SubEarningsSection(Section):
             (INSTITUTION, earnings_list[1], "", f"{prefix}_salaries_inst"),
             (DATA_FROM_PEOPLE, earnings_list[2], "", f"{prefix}_salaries_inst"),
             (NATIONAL_AVERAGE, earnings_list[3], "", f"{prefix}_salaries_sector"),
-            (SHOW_DATA, earnings_list[4], "", f"{prefix}_salaries_sector")
+            # (SHOW_DATA, earnings_list[4], "", f"{prefix}_salaries_sector")
         ]
 
     def get_sections(self) -> List[Tuple[Any, Any, Any, str]]:
@@ -41,22 +46,25 @@ class SubEarningsSection(Section):
         sections = [
             (
                 ("1", AVERAGE_EARNINGS, earnings_list[0], "", "go_salaries_inst"),
-                ("2", INSTITUTION, earnings_list[1], "", "go_salaries_inst"),
+                ("2", INSTITUTION, earnings_list[1], "£", "go_salaries_inst"),
                 ("3", DATA_FROM_PEOPLE, earnings_list[2], "", "go_salaries_inst"),
-                ("4", NATIONAL_AVERAGE, earnings_list[3], "", "go_salaries_sector"),
-                ("5", DATA_FROM_PEOPLE, earnings_list[4], "", "go_salaries_sector"),
+                ("4", NATIONAL_AVERAGE, earnings_list[3], "£", "go_salaries_sector"),
+                ("5", "", earnings_list[4], "", "go_salaries_sector"),
+                ("6", DATA_FROM_PEOPLE, earnings_list[5], "", "go_salaries_sector"),
 
-                ("6", AVERAGE_EARNINGS, earnings_list[0], "", "leo3_salaries_inst"),
-                ("7", INSTITUTION, earnings_list[1], "", "leo3_salaries_inst"),
-                ("8", DATA_FROM_PEOPLE, earnings_list[2], "", "leo3_salaries_inst"),
-                ("9", NATIONAL_AVERAGE, earnings_list[3], "", "leo3_salaries_sector"),
-                ("10", DATA_FROM_PEOPLE, earnings_list[4], "", "leo3_salaries_sector"),
+                ("7", AVERAGE_EARNINGS, earnings_list[0], "", "leo3_salaries_inst"),
+                ("8", INSTITUTION, earnings_list[1], "£", "leo3_salaries_inst"),
+                ("9", DATA_FROM_PEOPLE, earnings_list[2], "", "leo3_salaries_inst"),
+                ("10", NATIONAL_AVERAGE, earnings_list[3], "£", "leo3_salaries_sector"),
+                ("11", "", earnings_list[4], "", "go_salaries_sector"),
+                ("12", DATA_FROM_PEOPLE, earnings_list[5], "", "leo3_salaries_sector"),
 
-                ("11", AVERAGE_EARNINGS, earnings_list[0], "", "leo5_salaries_inst"),
-                ("12", INSTITUTION, earnings_list[1], "", "leo5_salaries_inst"),
-                ("13", DATA_FROM_PEOPLE, earnings_list[2], "", "leo5_salaries_inst"),
-                ("14", NATIONAL_AVERAGE, earnings_list[3], "", "leo5_salaries_sector"),
-                ("15", DATA_FROM_PEOPLE, earnings_list[4], "", "leo5_salaries_sector")
+                ("13", AVERAGE_EARNINGS, earnings_list[0], "", "leo5_salaries_inst"),
+                ("14", INSTITUTION, earnings_list[1], "£", "leo5_salaries_inst"),
+                ("15", DATA_FROM_PEOPLE, earnings_list[2], "", "leo5_salaries_inst"),
+                ("16", NATIONAL_AVERAGE, earnings_list[3], "£", "leo5_salaries_sector"),
+                ("17", "", earnings_list[4], "", "go_salaries_sector"),
+                ("18", DATA_FROM_PEOPLE, earnings_list[5], "", "leo5_salaries_sector")
             )
         ]
         sub_sections = []
@@ -72,6 +80,7 @@ class SubEarningsSection(Section):
         for section in self.sections:
             self.data[section[0]] = self.empty_data_structure(section[primary_key], self.language)
 
+
     def generate_dict(self) -> dict:
         for course in self.courses:
             for section in self.sections:
@@ -82,21 +91,48 @@ class SubEarningsSection(Section):
                         model_list=section[model_index],
                         language=self.language,
                         multiple=True,
-                        suffix=section[suffix_index]
+                        prefix=section[prefix_index]
                     )
                 )
-        # self.update_data_with_subtitles(self.data)
-
         return self.data
 
-    # def update_data_with_subtitles(self, data: Dict[str, Any]):
-    #     subtitles = {
-    #         "5": "unemp_prev_emp_since_grad",
-    #         "6": "unemp_not_work_since_grad",
-    #         "9": "employed_in_professional",
-    #         "10": "employed_not_in_professional",
-    #         "11": "employment_type_unknown"
-    #     }
-    #     for section in self.sections:
-    #         if section[0] in subtitles:
-    #             data[section[0]]["subtitle"] = self.term_for_key(subtitles[section[0]], self.language)
+    @classmethod
+    def multiple_subjects(cls, course: Course, stat: str, model_list: str, language: str, prefix=""):
+        response = dict(subject=[], values=[])
+
+        for index, subject in enumerate(course.subject_names):
+            subject_name = subject.display_subject_name()
+            values = translations.term_for_key(key="no_data_available", language=language)
+
+            if index < len(getattr(course, model_list)):
+                _object = getattr(course, model_list)[index]
+                method = str(getattr(_object, stat))
+                no_data = translations.term_for_key(key="no_data_available", language=language)
+                values = f"{prefix}{method}" if method else no_data
+
+            response["subject"].append(subject_name)
+            response["values"].append(values)
+        return response
+
+    @classmethod
+    def presentable_data(cls, course: Course, stat: str, model_list: str, language: str, multiple=False, prefix="") -> str:
+        response = translations.term_for_key(key="no_data_available", language=language)
+        try:
+            if multiple and course.has_multiple_subject_names:
+                response = cls.multiple_subjects(
+                    course=course,
+                    stat=stat,
+                    model_list=model_list,
+                    language=language,
+                    prefix=prefix
+                )
+            else:
+                _object = getattr(course, model_list)[0]
+                method = str(getattr(_object, stat))
+                if method:
+                    response = f"{prefix}{method}" if method.isnumeric() else method
+        except Exception as e:
+            print("error: ", e)
+            pass
+
+        return response
