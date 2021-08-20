ddb0aec9162df494be202d9e066824394a524727
nickpatti <nickdpatti50@gmail.com>
2021-08-20 11:54:29 +0100

    comment removed.

--- CMS/static/js/main.js
+++ CMS/static/js/main.js
@@ -126,7 +126,6 @@ const BookmarkEvent = new Event('bookmarkchanged');
 
                 if (this.selectedCourses.length > 0) {
                     this.btn.show();
-                    // this.btn.text(this.selectedCourses.length);
                 } else if (this.selectedCourses.length === 0) {
                     this.btn.hide();
                 }
