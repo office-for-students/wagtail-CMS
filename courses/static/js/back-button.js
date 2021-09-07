507699ac3ab70356f2f2005e3cb22eadafb3c3f3
Josh Ball <joshua.ball@mobilise.cloud>
2021-09-07 16:53:56 +0100

    fixes nav back button

--- courses/static/js/back-button.js
+++ courses/static/js/back-button.js
@@ -1,11 +1,5 @@
-$(document).ready(function() {
-    var btnVisibility = sessionStorage.getItem("btnVisibility");
-    
-    if(btnVisibility) {
-        var btn = document.getElementById("course-detail__nav-control-back").style.visibility = "visible";  
-        $("#course-detail__nav-control-back").click(function(evt) {  
-            sessionStorage.removeItem('btnVisibility');
-            window.history.back();
-        }); 
-    }
+$(document).ready(function () {
+    $("#course-detail__nav-control-back").click(function (evt) {
+        window.history.back();
+    });
 });
