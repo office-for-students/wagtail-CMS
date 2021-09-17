7f0fc807e422e952b4997f3e6f204b285a4f2f77
nickpatti <nickdpatti50@gmail.com>
2021-09-17 11:13:06 +0100

    Corrected alt text

--- courses/renderer/sections/information.py
+++ courses/renderer/sections/information.py
@@ -1,3 +1,4 @@
+from CMS import translations
 from courses.renderer.sections.base import Section
 
 
@@ -7,9 +8,10 @@ class InformationSection(Section):
         self.data['info'] = self.empty_data_structure('Link to website', self.language)
 
     def generate_dict(self) -> dict:
+        alt_text = translations.term_for_key(key="open_tab", language=self.language)
         for course in self.courses:
             link = course.course_links['course_details'][0].link
             self.data['info']['values'].append(
                 f"<a class='institution-link' href='{link}' target='_blank'>{course.institution_name}"
-                f"<img src='/static/images/new_tab_icon.png' alt='graphic'></a>")
+                f"<img src='/static/images/new_tab_icon.png' alt={alt_text}></a>")
         return self.data
