c23f05e03f6226b4fc47705d23f22ffaf5668a66
Josh Ball <joshua.ball@mobilise.cloud>
2021-09-28 15:05:03 +0100

    revert year hardcoding

--- courses/models/course.py
+++ courses/models/course.py
@@ -193,10 +193,10 @@ class Course:
                         self.default_region = region_elem['name_cy']
                         break
 
-            prefix = translations.term_for_key('average_earnings_year_range', self.display_language)
-            self.go_year_range = prefix + " {}-{}".format(2018, 2019)
-            self.leo3_year_range = prefix + " {}-{}".format(2011, 2013)
-            self.leo5_year_range = prefix + " {}-{}".format(2011, 2013)
+            prefix = translations.term_for_key('average_earnings_year_range', language)
+            self.go_year_range = prefix + " {}-{}".format(2017, 2018)
+            self.leo3_year_range = prefix + " {}-{}".format(2010, 2012)
+            self.leo5_year_range = prefix + " {}-{}".format(2010, 2012)
 
             self.go_salaries_inst = []
             if course_details.get('go_salary_inst'):
