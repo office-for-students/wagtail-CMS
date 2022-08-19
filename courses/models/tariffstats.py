d1aac2b3709b2604674314b8643f6bc7448f09ac
nickpatti <nickdpatti50@gmail.com>
2022-08-19 16:11:37 +0100

    unavailable messages overridden in required places (not earnings)

--- courses/models/tariffstats.py
+++ courses/models/tariffstats.py
@@ -1,7 +1,7 @@
 from CMS.enums import enums
 from .tariff import Tariff
 from .tariff import tariff_range
-from .utils import display_unavailable_info
+from .utils import display_unavailable_info, new_subject_unavail
 
 
 class TariffStatistics:
@@ -40,11 +40,18 @@ class TariffStatistics:
             self.unavailable_find_out_more_welsh = unavailable_data.get('find_out_more_welsh', '')
             self.unavailable_url_english = unavailable_data.get('url_english', '')
             self.unavailable_url_welsh = unavailable_data.get('url_welsh', '')
-            self.display_unavailable_info = display_unavailable_info(
-                self,
-                aggregation_level=self.aggregation_level,
-                subject_welsh=self.subject_welsh
-            )
+            if self.unavailable_code == 1 and self.aggregation_level in ["", 11, 12, 13, 21, 22, 23]:
+                self.display_unavailable_info = new_subject_unavail(
+                    aggregation_level=self.aggregation_level,
+                    subject_title_in_local_language=self.display_subject_name(),
+                    language=self.display_language
+                )
+            else:
+                self.display_unavailable_info = display_unavailable_info(
+                    self,
+                    aggregation_level=self.aggregation_level,
+                    subject_welsh=self.subject_welsh
+                )
             self.unavailable_reason_heading = self.display_unavailable_info["reason_heading"]
             self.unavailable_reason_body = self.display_unavailable_info["reason_body"]
             if str(self.aggregation_level) in ["11", "12", "13", "21", "22", "23"]:
