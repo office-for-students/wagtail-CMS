1e6a52190f43ac72127b5e425d036c0f79cbbb7e
nickpatti <nickdpatti50@gmail.com>
2022-09-27 11:12:37 +0100

    added welsh for leo banner

--- CMS/translations/dictionaries/general.py
+++ CMS/translations/dictionaries/general.py
@@ -1499,7 +1499,7 @@ DICT = {
     },
     'leo_banner': {
         'en': 'we anticipate that "Earnings after the course" data for 3 years and 5 years after graduation will be updated during November 2022 as new data is published.',
-        'cy': 'TBC'
+        'cy': 'rydym yn disgwyl y bydd data “Enillion ar ôl y cwrs” ar gyfer 3 blynedd a 5 mlynedd ar ôl graddio’n cael ei ddiweddaru yn ystod mis Tachwedd 2022 wrth i ddata newydd gael ei gyhoeddi. Darllenwch fwy.'
     },
     'please_note': {
         'en': 'Please note: ',
