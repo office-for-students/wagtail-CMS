962a25b0143fe7031c41cb53552b0e4bb60a35cd
SimonMobi <85573550+SimonMobi@users.noreply.github.com>
2023-05-22 17:08:17 +0100

    rmove cookie js from main.js

--- CMS/static/js/main.js
+++ CMS/static/js/main.js
@@ -140,35 +140,6 @@ const BookmarkEvent = new Event('bookmarkchanged');
         }
 
 
-        // COOKIE BANNER
-        var CookieBanner = function (wrapper) {
-            this.wrapper = $(wrapper);
-            this.setup();
-        }
-
-        CookieBanner.prototype = {
-            setup: function () {
-                this.acceptBtn = this.wrapper.find('.cookie-banner__ok');
-                this.findOutMoreBtn = this.wrapper.find('.cookie-banner__find-out-more');
-                if (localStorage.discoverUniCookies === 'accepted') {
-                    document.cookie = "discoverUniCookies=accepted;";
-                    this.wrapper.hide();
-                }
-
-                this.startWatchers();
-            },
-
-            startWatchers: function () {
-                var that = this;
-
-                this.acceptBtn.click(function () {
-                    document.cookie = "discoverUniCookies=accepted;";
-                    localStorage.discoverUniCookies = 'accepted';
-                    that.wrapper.hide();
-                });
-            }
-        }
-
         // SCROLL TO TOP BUTTON
         var ScrollToTop = function (scrollBtn) {
             this.scrollBtn = $(scrollBtn);
