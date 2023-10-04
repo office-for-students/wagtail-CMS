713d8ff3f2e6233b9a398d35aa0126d810c48f80
nickpatti <nickdpatti50@gmail.com>
2023-10-04 11:05:41 +0100

    agree or strongly agree is now none as fallback

--- courses/models/satisfactionquestion.py
+++ courses/models/satisfactionquestion.py
@@ -5,7 +5,7 @@ class SatisfactionQuestion:
         if question_data:
             self.show_data_point = 'agree_or_strongly_agree' in question_data
             self.description = question_data.get('description', '')
-            self.agree_or_strongly_agree = question_data.get('agree_or_strongly_agree', 0)
+            self.agree_or_strongly_agree = question_data.get('agree_or_strongly_agree')
 
     def __str__(self):
         if self.show_data_point:
