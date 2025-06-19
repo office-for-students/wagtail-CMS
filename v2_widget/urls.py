4ff686c30be426176740c9e48d41d2f84cd77898
simon.hurst <simon.hurst@mobilise.cloud>
2025-06-19 10:09:28 +0100

    add placeholder on guidance link

--- v2_widget/urls.py
+++ v2_widget/urls.py
@@ -7,6 +7,7 @@ from v2_widget.views import v2_widget_placeholder_embed
 
 urlpatterns = [
     path('configurator/', configurator_view, name='v2_widget_configurator'),
+path('configurator/guidance', configurator_view, name='v2_widget_configurator_guidance'),
     path('<placeholder>/<lang>', v2_widget_placeholder_embed, name='v2_widget_embed_placeholder'),
     path('<institution>/<course>/<mode>/<lang>', v2_widget_embed, name='v2_widget_embed'),
     path('api/<uni_id>/<course_id>/<mode>/<first_stat>/<second_stat>/<third_stat>', v2_api, name='v2_widget_api'),
