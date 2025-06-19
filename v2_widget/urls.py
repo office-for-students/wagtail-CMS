8170dd8dabd93eb01102606d109de12c87dafa04
simon.hurst <simon.hurst@mobilise.cloud>
2025-06-19 09:53:19 +0100

    update to include placeholder page for widget configurator

--- v2_widget/urls.py
+++ v2_widget/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from v2_widget.views import configurator_view
+from v2_widget.views import v2_api
 from v2_widget.views import v2_widget_embed
 from v2_widget.views import v2_widget_placeholder_embed
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('configurator/', configurator_view, name='v2_widget_configurator'),
     path('<placeholder>/<lang>', v2_widget_placeholder_embed, name='v2_widget_embed_placeholder'),
     path('<institution>/<course>/<mode>/<lang>', v2_widget_embed, name='v2_widget_embed'),
+    path('api/<uni_id>/<course_id>/<mode>/<first_stat>/<second_stat>/<third_stat>', v2_api, name='v2_widget_api'),
 ]
--- v2_widget/views.py
+++ v2_widget/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
 import requests
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
+
 """
 test endpoint: http://127.0.0.1:8000/widget/v2/10003614/BACTSINGW9F/FullTime/en
 """
 WIDGET_HOST = settings.V2_WIDGET_HOST
+
 
 def configurator_view(request):
     return render(request=request, template_name='v2_widget/configurator.html', context={'WIDGET_HOST': WIDGET_HOST})
@@ -17,19 +18,18 @@ def configurator_view(request):
 
 @xframe_options_exempt
 @csrf_exempt
-def v2_widget_embed(request, institution:str, course:str, mode:str, lang:str):
+def v2_widget_embed(request, institution: str, course: str, mode: str, lang: str):
     try:
-       return proxy_content(target_url=f"{WIDGET_HOST}widget/v2/{institution}/{course}/{mode}/{lang}",request=request)
+        return proxy_content(target_url=f"{WIDGET_HOST}widget/v2/{institution}/{course}/{mode}/{lang}", request=request)
 
     except requests.exceptions.RequestException as e:
         # TODO: check if in debug
         return HttpResponse(f"Error: {str(e)}", status=502)
 
 
-
 @xframe_options_exempt
 @csrf_exempt
-def v2_widget_placeholder_embed(request, placeholder:str, lang:str):
+def v2_widget_placeholder_embed(request, placeholder: str, lang: str):
     try:
         return proxy_content(target_url=f"{WIDGET_HOST}widget/v2/{placeholder}/{lang}", request=request)
 
@@ -38,8 +38,7 @@ def v2_widget_placeholder_embed(request, placeholder:str, lang:str):
         return HttpResponse(f"Error: {str(e)}", status=502)
 
 
-
-def proxy_content(request, target_url:str):
+def proxy_content(request, target_url: str, content_type="text/html"):
     print("method = ", request.method)
 
     headers = {
@@ -55,5 +54,25 @@ def proxy_content(request, target_url:str):
     return HttpResponse(
         content=response.content,
         status=response.status_code,
-        content_type=response.headers.get("Content-Type", "text/html")
+        content_type=response.headers.get("Content-Type", content_type)
     )
+
+
+def v2_api(
+        request,
+        uni_id: str,
+        course_id: str,
+        mode: str,
+        first_stat: str,
+        second_stat: str,
+        third_stat: str
+):
+    try:
+        return proxy_content(
+            target_url=f"{WIDGET_HOST}widget/v2/api/{uni_id}/{course_id}/{mode}/{first_stat}/{second_stat}/{third_stat}",
+            request=request,
+            content_type="application/json"
+        )
+
+    except requests.exceptions.RequestException as e:
+        return HttpResponse(f"Error: {str(e)}", status=502)
