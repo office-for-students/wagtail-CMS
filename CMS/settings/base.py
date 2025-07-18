a43ba27ac26a2700ecf3ff371681a2c2bb1053cb
simon.hurst <simon.hurst@mobilise.cloud>
2025-07-18 14:16:33 +0100

    use decouple for environment variables

--- CMS/settings/base.py
+++ CMS/settings/base.py
@@ -13,23 +13,24 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from corsheaders.defaults import default_headers
+from decouple import config
 # from decouple import config
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-LOCAL = True if os.environ.get('LOCAL', "") == "True" else False
-READ_ONLY = os.environ.get('READ_ONLY', False)
+LOCAL = True if config('LOCAL', "") == "True" else False
+READ_ONLY = config('READ_ONLY', False)
 
-ROOT_DOMAIN = os.environ.get('ROOT_DOMAIN', 'http://localhost:3000')
+ROOT_DOMAIN = config('ROOT_DOMAIN', 'http://localhost:3000')
 
 # Application definition
 
 INSTALLED_APPS = [
     'customadmin',
     'django.contrib.sitemaps',
-    'wagtail.contrib.forms',
-    'wagtail.contrib.modeladmin',
-    'wagtail.contrib.redirects',
+    'wagtail',
+    # 'wagtail.contrib.modeladmin',
+    # 'wagtail.contrib.redirects',
     # 'wagtail.contrib.frontend_cache',
     'wagtail.embeds',
     'wagtail.sites',
@@ -137,11 +138,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DBNAME'),
-            'HOST': os.environ.get('DBHOST', 'db'),
-            'USER': os.environ.get('DBUSER'),
-            'PORT': os.environ.get('DBPORT'),
-            'PASSWORD': os.environ.get('DBPASSWORD'),
+            'NAME': config('DBNAME'),
+            'HOST': config('DBHOST', 'db'),
+            'USER': config('DBUSER'),
+            'PORT': config('DBPORT'),
+            'PASSWORD': config('DBPASSWORD'),
             'SSL':True
         }
     }
@@ -211,33 +212,26 @@ WAGTAIL_SITE_NAME = "CMS"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = os.environ.get('ROOT_DOMAIN', 'http://example.com')
+BASE_URL = config('ROOT_DOMAIN', 'http://example.com')
 
 # Search API settings
 
-SORT_BY_SUBJECT_LIMIT = os.environ.get('SORT_BY_SUBJECT_LIMIT')
-SEARCHAPIHOST = os.environ.get('SEARCHAPIHOST')
-DATASETAPIHOST = os.environ.get('DATASETAPIHOST')
-DATASETAPIKEY = os.environ.get('DATASETAPIKEY')
-AZURECOSMOSDBURI = os.environ.get('AZURECOSMOSDBURI')
-AZURECOSMOSDBKEY = os.environ.get('AZURECOSMOSDBKEY')
-MONGODB_HOST = os.environ.get('MONGODB_HOST')
-MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
-MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
-TEST_COURSES = os.environ.get('TEST_COURSES')
-WIDGETAPIKEY = os.environ.get('WIDGETAPIKEY')
-WIDGETAPIHOST = os.environ.get('WIDGETAPIHOST')
-V2_WIDGET_HOST = os.environ.get('V2_WIDGET_HOST')
-FEEDBACK_API_HOST = os.environ.get('FEEDBACK_API_HOST')
-JSONFILES_STORAGE_CONTAINER = os.environ.get('JSONFILES_STORAGE_CONTAINER', "")
-SITEMAP_STORAGE_BLOB = os.environ.get('SITEMAP_STORAGE_BLOB', "")
-STORAGEKEY = os.environ.get('STORAGEKEY')
-STORAGE_ACCOUNT_NAME = os.environ.get('STORAGE_ACCOUNT_NAME')
+SORT_BY_SUBJECT_LIMIT = config('SORT_BY_SUBJECT_LIMIT')
+SEARCHAPIHOST = config('SEARCHAPIHOST')
+DATASETAPIHOST = config('DATASETAPIHOST')
+DATASETAPIKEY = config('DATASETAPIKEY')
+AZURECOSMOSDBURI = config('AZURECOSMOSDBURI')
+AZURECOSMOSDBKEY = config('AZURECOSMOSDBKEY')
+TEST_COURSES = config('TEST_COURSES')
+WIDGETAPIKEY = config('WIDGETAPIKEY')
+WIDGETAPIHOST = config('WIDGETAPIHOST')
+V2_WIDGET_HOST = config('V2_WIDGET_HOST')
+JSONFILES_STORAGE_CONTAINER = config('JSONFILES_STORAGE_CONTAINER', "")
+SITEMAP_STORAGE_BLOB = config('SITEMAP_STORAGE_BLOB', "")
+STORAGEKEY = config('STORAGEKEY')
+STORAGE_ACCOUNT_NAME = config('STORAGE_ACCOUNT_NAME')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME')  # eg. 'campaignstorage'
-AZURE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY')  # eg. '<secret key>'
-AZURE_CONTAINER = os.environ.get('AZURE_CONTAINER')  # eg. 'campaign-resource-centre'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
