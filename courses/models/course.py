4d7df54d7c3ddd88785d7e7455b570a6462ea775
simon.hurst <simon.hurst@mobilise.cloud>
2025-09-12 17:37:13 +0100

    range should be 22-23 not 21-23

--- courses/models/course.py
+++ courses/models/course.py
@@ -194,7 +194,7 @@ class Course:
                         break
 
             prefix = translations.term_for_key('average_earnings_year_range', language)
-            self.go_year_range = prefix + " {}-{}".format(2021, 23)
+            self.go_year_range = prefix + " {}-{}".format(2022, 23)
             self.leo3_year_range = prefix + " {}-{}".format(2014, 16)
             self.leo5_year_range = prefix + " {}-{}".format(2014, 16)
 
