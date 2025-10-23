d6c4d2776ed4f4e1170120acb307eb23fd24dd05
TomMobi <thomas.reed@mobilise.cloud>
2025-10-23 15:35:07 +0100

    Re-enable migration command on pipeline

--- deploy_files/docker-entrypoint.sh
+++ deploy_files/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-#until python3 manage.py migrate
-#do
-#  echo "Waiting for database to be migrated"
-#  sleep 5
-#done
-#
+until python3 manage.py migrate
+do
+  echo "Waiting for database to be migrated"
+  sleep 5
+done
+
 #until python3 manage.py update_index
 #do
 #  echo "Waiting for search to be indexed"
