5900f99e00276465f64de48ae2370a99284895cc
TomMobi <thomas.reed@mobilise.cloud>
2025-10-24 11:55:16 +0100

    Update wagtail module paths in remaining files
    Uses new paths introduced in 3.0; old paths are removed in 5.0.

--- content/management/commands/populate_cms.py
+++ content/management/commands/populate_cms.py
@@ -5,7 +5,7 @@ from core.command import SimpleCommand
 from django.contrib.contenttypes.models import ContentType
 from django.core import management
 from django.core.management.base import CommandError
-from wagtail.core.models import Page
+from wagtail.models import Page
 
 
 class Command(SimpleCommand):
--- core/signals.py
+++ core/signals.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from wagtail.wagtaildocs.models import Document
-from wagtail.wagtailimages.models import Image
+from wagtail.documents.models import Document
+from wagtail.images.models import Image
+
 from core.utils import check_for_virus
 
 if settings.CLAMAV_ACTIVE:
