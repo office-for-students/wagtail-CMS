79b6ecf988ad06b5782e6aa87a3b07db185f39d6
simon.hurst <simon.hurst@mobilise.cloud>
2025-11-06 14:56:14 +0000

    - use email address passed in to environment for From - fix wagtail admin base url

--- core/backends/azure_email.py
+++ core/backends/azure_email.py
@@ -25,7 +25,6 @@ class OFSEmailBackend(BaseEmailBackend):
         self.email_client = None
         try:
             self.connection_string = connection_string or settings.AZURE_EMAIL_SERVICE_CONNECTION_STRING
-            print(f"Connect String : {self.connection_string}")
             self.email_client = email_client or EmailClient.from_connection_string(self.connection_string)
         except ImproperlyConfigured:
             if not self.fail_silently:
@@ -58,29 +57,32 @@ class OFSEmailBackend(BaseEmailBackend):
         messages sent.
         """
         def send():
-            for message in email_messages:
-                try:
-                    if message.alternatives and message.alternatives[0].content:
-                        html_message = message.alternatives[0].content
-                    else:
+            if not settings.DEBUG:
+                for message in email_messages:
+                    try:
+                        if message.alternatives and message.alternatives[0].content:
+                            html_message = message.alternatives[0].content
+                        else:
+                            html_message = message.body
+                    except (IndexError, AttributeError):
                         html_message = message.body
-                except (IndexError, AttributeError):
-                    html_message = message.body
 
-                email_content = {
-                    "senderAddress": "DoNotReply@0733f806-c2b3-411c-9e73-eaef5c3155bb.azurecomm.net",
-                    "recipients": {
-                        "to": [{"address": x} for x in message.to]
-                    },
-                    "content": {
-                        "subject": f"[{settings.AZURE_EMAIL_SERVICE_ENVIRONMENT} - DU]  {message.subject}",
-                        "plainText": message.body,
-                        "html": html_message,
-                    },
-                }
+                    email_content = {
+                        "senderAddress": settings.AZURE_EMAIL_OUTGOING_EMAIL_ADDRESS,
+                        "recipients": {
+                            "to": [{"address": x} for x in message.to]
+                        },
+                        "content": {
+                            "subject": f"[{settings.AZURE_EMAIL_SERVICE_ENVIRONMENT} - DU]  {message.subject}",
+                            "plainText": message.body,
+                            "html": html_message,
+                        },
+                    }
 
-                poller = self.email_client.begin_send(email_content)
-                result = poller.result()
+                    poller = self.email_client.begin_send(email_content)
+                    result = poller.result()
+                else:
+                    logging.info(f"No Email message sent in DEBUG: {settings.DEBUG}. Message not sent: {email_content}.")
 
         thread = threading.Thread(target=send)
         thread.daemon = True
