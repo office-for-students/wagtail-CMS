7e886cb77e85ff70b662eb35469d872287e686a9
Simon Hurst <simon.hurst@mobilise.cloud>
2025-12-08 15:45:01 +0000

    - fix migration issue after upgrading to wagtail 6.x

--- CMS/settings/base.py
+++ CMS/settings/base.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'wagtail.contrib.search_promotions',
 
     # Wagtail
     'wagtail.contrib.forms',
