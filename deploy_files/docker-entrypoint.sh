b4a503bbd233fb73ae98b22a3d9842cc860ecc37
Simon Hurst <simon.hurst@mobilise.cloud>
2025-12-08 19:42:48 +0000

    - update the gunicorn process

--- deploy_files/docker-entrypoint.sh
+++ deploy_files/docker-entrypoint.sh
@@ -14,16 +14,35 @@ set -e
 #done
 
 #Run Gunicorn
+#exec gunicorn CMS.wsgi:application \
+#  --name discover-uni-cms \
+#  --bind 0.0.0.0:80 \
+#  --workers 4 \
+#  --threads 2 \
+#  --log-level=warning \
+#  --log-file=- \
+#  --access-logfile=- \
+#  --error-logfile=- \
+#  --timeout 60 \
+#  --graceful-timeout 30 \
+#  --max-requests 1000 \
+#  --max-requests-jitter 50
+
 exec gunicorn CMS.wsgi:application \
   --name discover-uni-cms \
-  --bind 0.0.0.0:80 \
-  --workers 3 \
-  --log-level=info \
-  --log-file=- \
+  --bind 0.0.0.0:8000 \
+  --worker-class gthread \
+  --workers 4 \
+  --threads 4 \
+  --preload \
+  --keep-alive 10 \
+  --log-level=warning \
   --access-logfile=- \
   --error-logfile=- \
   --timeout 60 \
-  --max-requests 1000
+  --graceful-timeout 30 \
+  --max-requests 5000 \
+  --max-requests-jitter 200
 
 
 # EXECUTE DOCKER COMMAND NOW
