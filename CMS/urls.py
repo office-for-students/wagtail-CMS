48012225431cb16d5e15495dc6f38db724ec4839
TomMobi <thomas.reed@mobilise.cloud>
2026-01-19 09:12:52 +0000

    - Install django debug toolbar
    Used to debug templates and context variables

--- CMS/urls.py
+++ CMS/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls import include
 from django.contrib import admin
@@ -66,3 +67,4 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.insert(0, path('__debug__/', include(debug_toolbar.urls)))
