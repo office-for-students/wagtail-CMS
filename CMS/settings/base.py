e89ad4ff2ad43f120ffd54f1c97705238d8efcc3
TomMobi <thomas.reed@mobilise.cloud>
2026-07-06 16:30:24 +0100

    - Upgrade wagtail to 7.4.2
    Upgrades wagtail to 7.4.2 and upgrades relevant dependencies

--- CMS/settings/base.py
+++ CMS/settings/base.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.postgres',
     'wagtail.contrib.search_promotions',
 
     # Wagtail
